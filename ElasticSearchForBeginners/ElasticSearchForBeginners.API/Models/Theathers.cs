1b90e87cbbaf06d2bce5117360c0ada8d0605e18
Buğra ÖÇALAN <bugraocalan@hotmail.com>
2019-12-14 00:14:50 +0300

    Model was created for books

--- ElasticSearchForBeginners/ElasticSearchForBeginners.API/Models/Theathers.cs
+++ ElasticSearchForBeginners/ElasticSearchForBeginners.API/Models/Theathers.cs
@@ -9,11 +9,7 @@ namespace ElasticSearchForBeginners.API.Models
     {
         public int ID { get; set; }
         public string Author { get; set; }
-        public string Name { get; set; }
-        public string Publisher { get; set; }
-        public decimal PublishYear { get; set; }
-        public bool IsHardCase { get; set; }
-        public decimal Price { get; set; }
+        public string Name { get; set; }             
         public string Summary { get; set; }
     }
 }
